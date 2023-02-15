7c8fcf70e909ac6d24e2ee6e0c345a9bf2aa599e
CristianUva <123639116+CristianUva@users.noreply.github.com>
2023-02-15 11:30:15 +0100

    Update esercizio_10.py

--- esercizio_10.py
+++ esercizio_10.py
@@ -1,4 +1,3 @@
-#Cristian Uva 3M 5/10/2022
 #Esercizio 10
 #Un cliente ha deciso di costruire una piccola casetta per gli attrezzi da giardino. Hai bisogno di L assi larghe per il tetto, P assi di legno piccole per le pareti e Q riquadri di legno per il pavimento. Realizza un programma che dopo avere preso in input L,P,Q e il loro prezzo unitario (senza IVA), calcoli e visualizzi la spesa da sostenere, sapendo che il venditore ha uno sconto del 10% ma chiede 30 euro per la consegna.
 
