46eca6411b4c12f299f374a83abf71606b31e257
SokolShkafi <122925258+SokolShkafi@users.noreply.github.com>
2023-03-02 13:28:31 +0100

    Add files via upload

---
+++ esercizio_034.py
@@ -0,0 +1,8 @@
+#il programma calcola il prodotto di due numeri interi
+num1=int(input("inserisci il primo numero intero: "))
+num2=int(input("inserisci il secondo numero intero: "))
+z=num1
+#ciclo
+for num2 in range(1, num2):
+    num1=num1+z
+print("il prodotto è", num1)
