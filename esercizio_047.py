e5290b37332db614c32e045e68c558d96a20c11f
GabrieleFranci <123639152+GabrieleFranci@users.noreply.github.com>
2023-03-02 13:48:44 +0100

    Add files via upload

---
+++ esercizio_047.py
@@ -0,0 +1,11 @@
+
+P = int(input("Inserisci il valore di N pari: "))
+
+for i in range(2, P*2+1, 2):
+    print(i)
+D = int(input("Inserisci il valore di N dispari : "))
+
+for i in range(1, (D*2+1)-1, 2):
+    print(i)
+
+
