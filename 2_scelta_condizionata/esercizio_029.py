c8259263cacdde7a1d583dc2296e55415152ebaa
Matteo Massa <124779943+MatteoMassa@users.noreply.github.com>
2023-03-08 22:43:26 +0100

    Add files via upload

---
+++ 2_scelta_condizionata/esercizio_029.py
@@ -0,0 +1,17 @@
+#Inizio
+print("Calcolare il costo di una vacanza:")
+
+gio = int(input("Inserire giorni di permanenza: "))
+
+if gio<=7:
+	sco= 600/gio
+	costo=100+(600-sco)+20
+	print("Il costo totale è: ", costo)
+
+elif gio<=14:
+	sco= 1100/gio
+	costo=100+(1100-sco)+40
+	print("Il costo totale è: ", costo)
+
+else:
+	print("Errore")
