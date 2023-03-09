3d333103e1a4d0428110be06548cee2ff1830f56
Mattia Lupascu <123638962+MattiaLupascu@users.noreply.github.com>
2023-03-09 13:40:42 +0100

    Delete esercizio_057.py

--- esercizio_057.py
+++
@@ -1,8 +0,0 @@
-#esercizio 57
-#definizione funzione descrizione()
-def descrizione(nome:str,eta:int) -> str:
-    return f"{nome} ha eta' {eta}"
-#main
-nome=input("Inserisci nome: ")
-eta=int(input("Inserisci l'eta': "))
-print(descrizione(nome,eta))
