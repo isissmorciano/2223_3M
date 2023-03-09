c8db745347050f8dbf3a32d7fa72b7ee44a03fd4
Matteo Pelusi <123639223+MatteoPelusi@users.noreply.github.com>
2023-03-09 13:42:53 +0100

    Add files via upload

---
+++ 5_funzioni/esercizio_065.py.py
@@ -0,0 +1,19 @@
+#Pelusi Matteo 3M
+#es 3
+
+#definisco la funzione differenza
+def differenza(num1, num2, num3):
+    Vmin = min(num1, num2, num3)
+    Vmax = max(num1, num2, num3)
+    return Vmax - Vmin
+
+#Main
+# richiesta informazioni
+num1 = int(input("Inserisci il primo numero intero: "))
+num2 = int(input("Inserisci il secondo numero intero: "))
+num3 = int(input("Inserisci il terzo numero intero: "))
+
+# Calcolo della differenza 
+differenza_num = differenza(num1, num2, num3)
+#stampa a schermo
+print("La differenza è:",differenza_num)
