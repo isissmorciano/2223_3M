9fe29ffdff844ee234cc168c0aebd652d2fbfe84
Gabriele Franci <123639152+GabrieleFranci@users.noreply.github.com>
2023-03-09 13:28:44 +0100

    Add files via upload

---
+++ 4_liste/esercizio_055.py
@@ -0,0 +1,16 @@
+#.Data in input una lista dei soli nominativi femminili, dire quanti sono i nomi femminili e generare una nuova lista con i nomi femminili e il volitivo esempio: "Sig.ra Maria" e visualizzarlo.
+print("*******************************************************************************************")
+print("| Dice quanti sono i nomi femminili e generare una nuova lista con i nomi femminili       |")
+print("*******************************************************************************************")
+
+
+#Nomi nella lista
+Lista_Nomi=["Signora Marina", "Signor Luigi", "Signora Sara ", "Signor Eugenio", "Signor Annibale", "Signora Lara", "Signora Barbara", "Signor Gabriele", "Signora Graziana", "Signor Mirco"]
+
+Nuova_Lista = [x for x in Lista_Nomi if "Signora" in x ]
+
+
+
+print("####################################################################################################################################################")
+for i in(Nuova_Lista):
+    print("Sig.ra", i[8:])
