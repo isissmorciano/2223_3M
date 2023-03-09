d829bf8b6da203f5a5616ec4bab8288c4d43de02
Gabriele Franci <123639152+GabrieleFranci@users.noreply.github.com>
2023-03-09 13:20:06 +0100

    Add files via upload

---
+++ 3_cicli/esercizio_051.py
@@ -0,0 +1,12 @@
+print("**************************************************************************************************************************************")
+print("|                     Leggi dalla tastiera una serie di coppie di numeri interi, una coppia per ogni riga.                             | ")        
+print("|Conta le coppie che presentano due valori che siano uguali. La serie termina quando entrambi ii valori letti in input sono uguali a 0.|")
+print("**************************************************************************************************************************************")
+count = 0
+while True:
+    x= int(input()) 
+    y = int(input())
+    if x == 0 and y == 0:
+        break
+    if x == y:
+        count += 1
