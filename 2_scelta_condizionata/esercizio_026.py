9bd34fecb821e1cc78113fabbb644e76b13dd965
Salvatore Ciaramidaro <123639341+SalvatoreCiaramidaro@users.noreply.github.com>
2023-03-09 13:46:45 +0100

    Add files via upload

---
+++ 2_scelta_condizionata/esercizio_026.py
@@ -0,0 +1,24 @@
+#Es n.26 Salvatore Ciaramidaro 3^M
+
+#Inserimento input
+nominativo = input("Inserire il proprio nome e cognome: ")
+voto1 = int(input("Inserire un voto da 1 a 10: "))
+voto2 = int(input("Inserire un voto da 1 a 10: "))
+voto3 = int(input("Inserire un voto da 1 a 10: "))
+voto4 = int(input("Inserire un voto da 1 a 10: "))
+
+
+#Calcolo media dei 4 voti
+media = ((voto1 + voto2 + voto3 + voto4)/4)
+
+#Determinazione media sufficiente o insufficiente
+#La media è sufficiente se è compreso tra 6 e 10
+if (media>=6) and (media<=10):
+	print("Studente: ",nominativo)
+	print("Media: ",media)
+	print("Media sufficiente")
+#La media è insufficiente se è compresa tra 1 e minore di 6
+elif (media>=1) and (media<6):
+	print("Studente: ",nominativo)
+	print("Media: ",media)
+	print("Media insufficiente")
