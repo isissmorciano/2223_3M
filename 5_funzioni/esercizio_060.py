bfdbe9ec3bb8028566c1e3a3d0bd2e553b5bfb9a
Gabriele Franci <123639152+GabrieleFranci@users.noreply.github.com>
2023-03-09 13:53:40 +0100

    Add files via upload

---
+++ 5_funzioni/esercizio_060.py
@@ -0,0 +1,9 @@
+#Creare una funzione(?) descrizione_casuale(). Il nome e' scelto in modo casuale da una lista di nomi interna alla fuzione. L'eta e' calcolata in modo casuale La funzione restituisce una stringa formattata nel seguente modo: "nome ha eta anni." descrizione_casuale()
+import random
+lista = ["giorgio" , "franci", "gregorio","Manuele"]
+def scelta():
+    nome=random.choice(lista)
+    eta = random.randint(1,199)
+    return nome , "ha eta ", eta
+s = scelta()
+print(s)
