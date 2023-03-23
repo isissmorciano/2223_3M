1caf6bf9ecdd6d3dd1d5cee496a376ff767a7a6c
Salvatore Ciaramidaro <salvatore.ciaramidaro@studenti.isissgobetti.it>
2023-03-23 13:25:29 +0100

    Esercizio 049

--- 3_cicli/esercizio_049.py
+++ 3_cicli/esercizio_049.py
@@ -1,19 +1,19 @@
-print("*******************************************************************************************")
-print("|                Questo programma ti scrive il nome del cd con durata maggiore                 |")
-print("*******************************************************************************************")
+print(f'*******************************************************************************************')
+print(f'|                Questo programma ti scrive il nome del cd con durata maggiore                 |')
+print(f'*******************************************************************************************f')
 
 
-N=int(input("Quanti cd vuoi prendere in considerazione?"))
+N=int(input(f'Quanti cd vuoi prendere in considerazione? '))
 max=0
 
 for i in range(N):
-    titolo=str(input("Inserisci titolo cd: "))
+    titolo=str(input(f'Inserisci titolo cd: '))
     nb=int(input("Quanti brani compongono il cd? "))
 
     
     for i in range(nb):
-        tb=str(input("Inserisci il titolo del brano: "))
-        db=int(input("Inserisci la durata del brano in minuti: "))
+        tb=str(input(f'Inserisci il titolo del brano: '))
+        db=int(input(f'Inserisci la durata del brano in minuti: '))
 
 
         if db>max:
