44cfdc78ac7c0482961963fc6b530b9395f39e11
Angelo Galanti <angelo.galanti@isissgobetti.it>
2023-03-23 12:58:13 +0100

    eliminato uno spazio superfluo

--- 1_input_output/esercizio_001.py
+++ 1_input_output/esercizio_001.py
@@ -2,7 +2,7 @@
 #Dato un importo calcolare lo sconto del 5% e applicare un IVA dell'8%
 
 #Scritta a schermo di cosa fa il programma
-print ("Inserisci un importo per applicargli lo sconto dell'5%, e poi le tasse dell'8% ")
+print ("Inserisci un importo per applicargli lo sconto dell'5%, e poi le tasse dell'8%")
 
 #Inserimento dell'importo
 importo = int(input("Inserisci l'importo: "))
