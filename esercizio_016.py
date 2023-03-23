5acda1ff5c1b918141672ab1eba3b29cc513ef21
Vullnet Sejdi <vullnet.sejdi@studenti.isissgobetti.it>
2023-03-23 13:17:17 +0100

    I maiuscola

--- esercizio_016.py
+++ esercizio_016.py
@@ -1,6 +1,6 @@
 #Inizio
 #spiegazione
-print("Sul prezzo di un prodotto viene applicato il 15%  di sconto se acquista un massimo di 10 prodotti ed un 20%  se qcquista più di 10 prodotti")
+print("Sul prezzo di un prodotto viene applicato il 15%  di sconto se acquista un massimo di 10 prodotti ed un 20%  se acquista più di 10 prodotti")
 x=int(input("quanti prodotti vuoi acquistare "))
 if x>10:
 	p=int(input("quanto costa il prodotto "))
